76f0c384e49ce29d6e76a054898aba444f3df6db
mactorient <mertemir38@hotmail.com>
2021-07-16 15:20:28 +0300

    small fixes and renamed variables

--- main.py
+++ main.py
@@ -1,29 +1,44 @@
 # ------------------------------------------------
 # * Created by github.com/mactorient
-# ? This tool gets pptp vpn password from vpnbook
+# * This tool gets pptp vpn password from vpnbook
 # ------------------------------------------------
 
-from pyperclip import copy as copy_to_clipboard
-from pytesseract import image_to_string as ocr # for requested
-from PIL import Image
-import requests
-import tempfile
+try:
+    from pyperclip import copy as copy_to_clipboard
+    from pytesseract import image_to_string as ocr # To extract the password from the photo
+    from PIL import Image
+    import requests
+    import tempfile
+except ImportError as e:
+    print("[ERROR] Import Error:", e)
+    quit(0)
 
 def get_password():
-    _file= tempfile.NamedTemporaryFile(mode='r+', prefix='vpnbook_', suffix='.png').name
-    img_url= 'https://www.vpnbook.com/password.php'
-    img_data= requests.get(img_url).content
-    with open(_file, 'wb') as f:
-        f.write(img_data)
-    _password= ocr(Image.open(_file)).replace('\n', '')
-    return _password
+    global PASSWORD
+    TEMP_FILE= tempfile.NamedTemporaryFile(mode='r+', suffix='.png').name
+    URL= 'https://www.vpnbook.com/password.php'
+
+    IMAGE_DATA= requests.get(URL).content
+    with open(TEMP_FILE, 'wb') as file: file.write(IMAGE_DATA)
+    
+    IMAGE= Image.open(TEMP_FILE)
+    PASSWORD= ocr(IMAGE).replace('\n', '')
+    
+    return PASSWORD
+
 
 def main():
     try: 
         password= get_password()
-        copy_to_clipboard(password)
-        print('Password copied to clipboard: {}'.format(password))
-    except Exception as e: print('Something went wrong:', e)
+        print("[SYSTEM] PPTP Password: %s" % password)
+    except Exception as e:
+        print('[ERROR] Something went wrong:', e)
+    else:
+        copy = input("[SYSTEM] Do you want to copy password to clipboard? (y/n): ")
+        if copy.strip().lower()=="y":
+            copy_to_clipboard(password)
+            print("[CLIPBOARD] Copied: %s" % password)
+
 
 if __name__=='__main__':
     main()
